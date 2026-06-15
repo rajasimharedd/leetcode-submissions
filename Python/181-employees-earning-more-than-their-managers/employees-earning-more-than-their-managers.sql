2df791ccf08a6176720ff01e9d8ccd880dc1618a
rajasimharedd <131618983+rajasimharedd@users.noreply.github.com>
2026-06-16 00:17:50 +0530

    Time: 477 ms (14.99%) | Memory: 0B (100.00%) - LeetSync

--- Python/181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ Python/181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select e1.name as employee from employee e1 left join employee e2 
+select e1.name as employee from employee e1 join employee e2 
 on e1.managerid = e2.id where e1.salary > e2.salary
