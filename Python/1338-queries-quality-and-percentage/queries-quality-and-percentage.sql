ae8a316690d181acd95a6b8b6896686261b64cb6
rajasimharedd <131618983+rajasimharedd@users.noreply.github.com>
2026-06-16 15:00:50 +0530

    Time: 431 ms (12.05%) | Memory: 0B (100.00%) - LeetSync

--- Python/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ Python/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select query_name, round((sum(rating/position)/count(query_name)),2) as quality,
-IFNULL(round(100*(select (count(*)/(select count(*) from queries q1 where q1.query_name = q2.query_name group by query_name)) 
-from queries q2 where rating<3 and q2.query_name = q3.query_name group by q2.query_name),2),0) as poor_query_percentage
+select 
+query_name, 
+round((sum(rating/position)/count(query_name)),2) as quality,
+IFNULL(round(100*(select (count(*)/(select count(*) from queries q1 where q1.query_name = q2.query_name group by query_name)) from queries q2 where rating<3 and q2.query_name = q3.query_name group by q2.query_name),2),0) as poor_query_percentage
  from queries q3 group by query_name
