b73f34033c57c77d5d6c115140d46c46c0a4b298
rajasimharedd <131618983+rajasimharedd@users.noreply.github.com>
2026-06-16 14:23:35 +0530

    Time: 533 ms (51.20%) | Memory: 0B (100.00%) - LeetSync

---
+++ Python/1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select 
+project_id,
+round(sum(e.experience_years)/count(e.employee_id),2) as average_years 
+from project p 
+left join employee e 
+    on e.employee_id = p.employee_id 
+group by project_id
+    
+    
