ed4c140cc0797d6d581997def85128471b794ccb
rajasimharedd <131618983+rajasimharedd@users.noreply.github.com>
2026-06-16 14:45:21 +0530

    Time: 982 ms (57.66%) | Memory: 0B (100.00%) - LeetSync

--- Python/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ Python/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select contest_id, round((100*count(contest_id)/(select count(*) from users)),2) as percentage from users u 
-right join register r on u.user_id = r.user_id 
+select contest_id, round((100*count(contest_id)/(select count(*) from users)),2) as percentage 
+from register 
 group by contest_id
 order by percentage desc, contest_id asc
