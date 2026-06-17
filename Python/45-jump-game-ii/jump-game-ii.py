aa59148109a734e23566671e0a1a479f5e5f3cdd
rajasimharedd <131618983+rajasimharedd@users.noreply.github.com>
2026-06-17 18:52:32 +0530

    Time: 4 ms (69.80%) | Memory: 20.1 MB (66.80%) - LeetSync

--- Python/45-jump-game-ii/jump-game-ii.py
+++ Python/45-jump-game-ii/jump-game-ii.py
@@ -1,12 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        count = 0
-        start = 0
-        current = 0
-        n = len(nums)-1
+        reach = 0 #how far i can reach from current pos
+        count = 0 # how many jumps taken so far.
+        last_reached = 0 # where did i jump last to. 
+        
+        if len(nums)==1:
+            return 0
         for i in range(len(nums)-1):
-            start = max(start, i+nums[i])
-            if i == current:
+            reach = max(reach, i+nums[i])
+            
+            if i == last_reached:
+                last_reached = reach
                 count+=1
-                current = start
-        return count 
+            
+                if reach >= len(nums)-1:
+                    return count
